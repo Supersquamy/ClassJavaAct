6b0b3823e39cfcf164e1d0d99fb9459245e7759d
Supersquamy <amyrobinsonlearns@gmail.com>
2020-08-12 20:10:28 +0100

    Wrote Knitting file

---
+++ ClassJavaAct-master/Knitting.java
@@ -0,0 +1,29 @@
+
+
+public class Knitting{
+
+    int castOn;
+    double finishedProjectLength;
+    String needleType;
+    String needleAvailable;
+    int projectTime;
+    
+        public Knitting(int castStitches, double lengthCm, String needleNeeded, String needleToHand, int timeRequired){
+            castOn = castStitches;
+            finishedProjectLength = lengthCm;
+            needleType = needleNeeded;
+            needleAvailable = needleToHand;
+            projectTime = timeRequired;
+        }
+    
+        public static void main(String[] args){
+            Knitting pattern = new Knitting(60, 90.4, "five millimetre", "seven millimetre", 180);
+            System.out.println("First, cast on " + pattern.castOn + " stitches.");
+            System.out.println("Your finished project will be " + pattern.finishedProjectLength + " cm long");
+            System.out.println("You need a " + pattern.needleType + " needle");
+            System.out.println("Can you complete the pattern with what you have available?");
+            System.out.println(pattern.needleAvailable == pattern.needleType);
+            System.out.println("You'll have completed this pattern in " + pattern.projectTime + " hours - keep going!");
+        }
+    
+    }
